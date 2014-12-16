b5701ffdc023e99de79a8d4220dec0c8b8775135
s-oku <oku@realglobe.jp>
2014-12-16 19:41:52 +0900

    template_params path/dest_path to absolute_path

--- app/controllers/concerns/response_json_builder.rb
+++ app/controllers/concerns/response_json_builder.rb
@@ -4,8 +4,16 @@ module ResponseJsonBuilder
   def build(template_name, **params)
     status_code = params[:status] || 200
     message = params.delete(:message) || I18n.t("json.template.#{template_name}.message")
-    descriptions = params.delete(:descriptions) ||
-      [I18n.t("json.template.#{template_name}.description", params.delete(:template_params))]
+    descriptions = params.delete(:descriptions)
+    if !descriptions
+      template_params = params.delete(:template_params)
+      %i(path dest_path).each do |path_params|
+        if path_value = template_params.try(:[], path_params)
+          template_params[path_params] = File.absolute_path("/./#{path_value}")
+        end
+      end
+      descriptions = [I18n.t("json.template.#{template_name}.description", template_params)]
+    end
     {
       status_code: status_code,
       message: message,
