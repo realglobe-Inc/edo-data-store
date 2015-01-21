1f45f02ead39ef96c873b0ba070d7f31702b379e
s-oku <oku@realglobe.jp>
2015-01-21 10:10:08 +0000

    bug fix Unicorn::WorkerKiller::Oom limit

--- config/initializers/unicorn_worker_killer.rb
+++ config/initializers/unicorn_worker_killer.rb
@@ -2,5 +2,5 @@ require 'unicorn/worker_killer'
 
 EdoPersonalCloud::Application.configure do
   config.middleware.use Unicorn::WorkerKiller::MaxRequests, 3072, 4096
-  config.middleware.use Unicorn::WorkerKiller::Oom, (1 * (1024 ** 2)), (2 * (1024 ** 2))
+  config.middleware.use Unicorn::WorkerKiller::Oom, (128 * (1024 ** 2)), (256 * (1024 ** 2))
 end
